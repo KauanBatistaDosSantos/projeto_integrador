fe8f535a4b96adc9e4b100df25a58e3207fbcd9b
Kauan Batista ADS-B <kauan18br@gmail.com>
2025-05-09 21:43:46 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TelaInicialComponent } from './components/tela-inicial/tela-inicial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: TelaInicialComponent}
+];
