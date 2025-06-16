38682ee5a5743f0682874cdff6f036975a5406ac
schneiker <vitorschneiker@gmail.com>
2025-06-16 17:32:10 -0300

    Finalizada estilização e comunicação ao supabase

--- src/app/components/tela-inicial/tela-inicial.component.ts
+++ src/app/components/tela-inicial/tela-inicial.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tela-inicial',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,      // IMPORTANTE para usar *ngFor, *ngIf, etc.
+    HttpClientModule   // para usar HttpClient
+  ],
   templateUrl: './tela-inicial.component.html',
-  styleUrl: './tela-inicial.component.css'
+  styleUrls: ['./tela-inicial.component.css']
 })
-export class TelaInicialComponent {
+export class TelaInicialComponent implements OnInit {
+  eventos: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:8080/eventos').subscribe(data => {
+      this.eventos = data;
+    });
+  }
 }
